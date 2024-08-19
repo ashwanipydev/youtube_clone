8cc55eb6dfe5218cfc7e12b622c47641a4693431
Ashwanipydev <pydev.ashwani@gmail.com>
2024-08-19 21:10:59 +0530

    commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { selectClasses, Stack } from "@mui/material";
+import { categories } from "../utils/constants";
+
+const Sidebar = ({selectedCategory, setSelectedCategory}) => (
+  <Stack
+    direction="row"
+    sx={{
+      overflow: "auto",
+      height: {
+        sx: "auto",
+        md: "95%",
+      },
+      flexDirection: {
+        md: "column",
+      },
+    }}
+  >
+    {categories.map((category) => (
+      <button
+        onClick={()=> setSelectedCategory(category.name)}
+        className="category-btn"
+        style={{
+          background: category.name === selectedCategory && "#fc1503",
+          color: "white",
+        }}
+        key={category.name}
+      >
+        <span
+          style={{
+            color: category.name === selectedCategory ? "white" : "red",
+            marginRight: "15px",
+          }}
+        >
+          {category.icon}
+        </span>
+        <span
+          style={{ opacity: category.name === selectedCategory ? "1" : ".8" }}
+        >
+          {category.name}
+        </span>
+      </button>
+    ))}
+  </Stack>
+);
+
+export default Sidebar;
---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import Navbar from "./Navbar";
+import ChannelDetail from "./ChannelDetail";
+import Feed from "./Feed";
+import SearchFeed from "./SearchFeed";
+import VideoDetail from "./VideoDetail";
+import SearchBar from "./SearchBar";
+import Sidebar from "./Sidebar";
+import Videos from "./Videos";
+
+export { Navbar, ChannelDetail, Feed, SearchFeed, VideoDetail, SearchBar, Sidebar , Videos };
